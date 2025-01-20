468d71e18a25ce72f3038ac04cc031ee7318b6fc
Flog7877 <flog7877@gmail.com>
2025-01-20 04:50:20 +0100

    Changelog

--- frontend/src/components/ChangelogContent.jsx
+++ frontend/src/components/ChangelogContent.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 const ChangelogContent = () => (
     <section className="changelog-text">
         <h4>
+            <span className="popup-version-badge">v0.1.3</span>
+        </h4>
+        <ul className="features" >
+            <li>Es gibt jetzt einen Handymodus! Es werden nun auch kleine Bildschirme unterstützt.</li>
+            <li>Der Fullscreen-Modus funktioniert jetzt (einigermaßen) vernünftig.</li>
+        </ul>
+        <h4>
             <span className="popup-version-badge">v0.1.2</span>
         </h4>
         <ul className="features" >
