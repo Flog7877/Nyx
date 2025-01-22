0bb2f851ed1890a5ea9d468d01e8d288e90561a0
Flog7877 <flog7877@gmail.com>
2025-01-22 03:50:51 +0100

    Config für Reverse Proxy eingerichtet

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import fs from 'fs';
 
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: {
-      key: fs.readFileSync('./192.168.0.51-key.pem'),
-      cert: fs.readFileSync('./192.168.0.51.pem'),
-    },
-    proxy: {
-      '/api': {
-        target: 'http://192.168.0.51:3001',
-        changeOrigin: true,
-        secure: false,
-      },
+    hmr: {
+      host: 'dev.flo-g.de', 
+      protocol: 'wss',       
+      port: 5173,            
     },
   },
   plugins: [
